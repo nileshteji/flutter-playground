9fd93b3914bbc9a2979bb6e194195c50e8fe50be
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-03-28 20:33:40 +0530

    Version 1.1

--- quiz_game/lib/main.dart
+++ quiz_game/lib/main.dart
@@ -8,20 +8,36 @@ void main(List<String> args) {
 class Application extends StatelessWidget{
   List<String> nilesh;
   int counter=0;
+
   Application({this.nilesh});
+ 
+ 
+
 
   @override
   Widget build(BuildContext context) {
   return MaterialApp(
   home: Scaffold(
-     body:Center(child:Text('$counter',style:TextStyle(color: Colors.black),textScaleFactor: 2,),),
-     drawer: Drawer(child: Text("nilesh"),),
+    appBar: AppBar(title:Text("Quiz Game"),backgroundColor:Colors.black),
+     body:Column(children: <Widget>[
+    
+       //AppBar(title:Text("Quiz Game",),backgroundColor: Colors.black,),
+           Row(children:<Widget>[]),
+           
+       RaisedButton(child:Text("Teji"))
+       
+       ], 
+      
+       )
+
+     
     
     
 
       
     
-  ));
+  )
+);
   }
 
 }
